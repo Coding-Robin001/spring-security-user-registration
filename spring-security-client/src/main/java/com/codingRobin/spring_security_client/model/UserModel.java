8a8135c6b82ed546463f5b36e938fbf3ab32c664
Coding-Robin001 <codingade@gmail.com>
2024-10-07 18:18:31 +0100

    added user model class ro model package and properties

--- spring-security-client/src/main/java/com/codingRobin/spring_security_client/model/UserModel.java
+++ spring-security-client/src/main/java/com/codingRobin/spring_security_client/model/UserModel.java
@@ -1,4 +1,9 @@
 package com.codingRobin.spring_security_client.model;
 
 public class UserModel {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String matchingPassword;
 }
