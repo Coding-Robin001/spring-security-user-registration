f3b632d602ceb3da643550ed7553f0d3049d3fe5
Coding-Robin001 <codingade@gmail.com>
2024-10-17 10:38:34 +0100

    commit

--- spring-security-client/src/main/java/com/codingRobin/spring_security_client/controller/RegistrationController.java
+++ spring-security-client/src/main/java/com/codingRobin/spring_security_client/controller/RegistrationController.java
@@ -4,6 +4,8 @@ import com.codingRobin.spring_security_client.entity.User;
 import com.codingRobin.spring_security_client.model.UserModel;
 import com.codingRobin.spring_security_client.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,13 @@ public class RegistrationController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @PostMapping("/register")
     public String registerUser(@RequestBody UserModel userModel){
         User user = userService.registerUser(userModel);
+        publisher.publishEvent();
         return "Registration Successful!";
     }
 }
