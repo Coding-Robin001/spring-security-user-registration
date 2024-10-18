e0d2e6a4149057eb28b31f6e59035555fa12f578
Coding-Robin001 <codingade@gmail.com>
2024-10-18 11:24:03 +0100

    verification token repo created

--- spring-security-client/src/main/java/com/codingRobin/spring_security_client/repository/VerficationTokenRepository.java
+++ spring-security-client/src/main/java/com/codingRobin/spring_security_client/repository/VerficationTokenRepository.java
@@ -1,9 +1,9 @@
 package com.codingRobin.spring_security_client.repository;
 
-import com.codingRobin.spring_security_client.entity.User;
+import com.codingRobin.spring_security_client.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface VerficationTokenRepository extends JpaRepository<VerificationToken, Long> {
 }
